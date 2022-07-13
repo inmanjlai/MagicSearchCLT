7ab686d0977f3c32f590bb954fffb90e97b87136
Ismail <inmanjlai@hotmail.com>
2022-07-12 23:33:16 -0400

    fixed spacing

--- scryfall.py
+++ scryfall.py
@@ -11,10 +11,10 @@ def getCard():
 
         # print out the details of the searched for card ->
         print(f"""
-    Card Name:\n    {card["name"]}\n\n
-    Oracle Text:\n  {card["oracle_text"]}\n\n
-    Mana Cost:\n    {card["mana_cost"]}\n\n
-    Type:\n     {card["type_line"]}\n\n
+Card Name:\n{card["name"]}\n\n
+Oracle Text:\n{card["oracle_text"]}\n\n
+Mana Cost:\n{card["mana_cost"]}\n\n
+Type:\n{card["type_line"]}\n\n
         """)
 
         repeat = input("Enter 'y' to look for another card, or 'n' to exit.\n")
@@ -24,6 +24,10 @@ def getCard():
         return
     else:
         print("No card with that name found")
-        getCard()
+        repeat = input("Enter 'y' to look for another card, or 'n' to exit.\n")
+        if repeat == 'y':
+            os.system("cls" if os.name == 'nt' else 'clear')
+            getCard()
+        return
 
 getCard()
